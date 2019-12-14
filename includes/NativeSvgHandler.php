bd604a699d319d30b8ab203ec4757bdda88c9667
alistair3149 <alistair31494322@gmail.com>
2019-12-13 21:24:43 -0500

    Added original config back

--- includes/NativeSvgHandler.php
+++ includes/NativeSvgHandler.php
@@ -41,52 +41,16 @@ class NativeSvgHandler extends SvgHandler {
             return new TransformParameterError( $params );
         }
 
-        global $wgNativeSvgHandlerEnableLinks;
-        if(!isset($wgNativeSvgHandlerEnableLinks) || $wgNativeSvgHandlerEnableLinks) {
+        if( $this->getConfig()->get('NativeSvgHandlerEnableLinks') == true ) {
             return new ThumbnailImage($image, $image->getURL(), $params['width'],
                                       $params['height'], $image->getPath() );
         }
+        
         return new SvgImage($image, $image->getURL(), $params['width'],
                             $params['height'], $image->getPath() );
     }
 
     function getThumbType($ext, $mime, $params = null) {
         return array( 'svg', 'image/svg+xml' );
-    }
-}
-
-class SvgImage extends MediaTransformOutput {
-
-    function __construct( $file, $url, $width, $height, $path = false, $page = false ) {
-        $this->file = $file;
-        $this->url = $url;
-
-        $this->width = round( $width ); //paranoid
-        $this->height = round( $height ); //paranoid
-
-        $this->path = $path;
-        $this->page = $page;
-    }
-
-    function toHtml( $options = array() ) {
-        if ( count( func_get_args() ) == 2 ) {
-            throw new MWException( __METHOD__ .' called in the old style' );
-        }
-
-        $alt = empty( $options['alt'] ) ? '' : $options['alt'];
-
-        $attribs = array(
-            'alt' => $alt,
-            'src' => $this->url,
-            'width' => $this->width,
-            'height' => $this->height,
-        );
-        if ( !empty( $options['valign'] ) ) {
-            $attribs['style'] = "vertical-align: {$options['valign']}";
-        }
-        if ( !empty( $options['img-class'] ) ) {
-            $attribs['class'] = $options['img-class'];
-        }
-        return Xml::element('img', $attribs);
     }
 }
